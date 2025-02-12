eb7f4a8a79528bfd101b6ce46b44f9431b36a0ba
Debankur04 <134576718+Debankur04@users.noreply.github.com>
2025-02-12 22:18:26 +0530

    Add files via upload

---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    signInWithPopup, 
+    GoogleAuthProvider 
+} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, googleProvider };
